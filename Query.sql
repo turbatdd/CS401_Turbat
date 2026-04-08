8d699ba171b5bee61616cdca10494a94c2a5302f
turbatdd <49607815+turbatdd@users.noreply.github.com>
2026-04-07 23:49:29 -0500

    lab7 Done

---
+++ Query.sql
@@ -0,0 +1,7 @@
+
+CREATE TABLE Address (
+                         address_id INT PRIMARY KEY,
+                         city VARCHAR(50) NOT NULL,
+                         state CHAR(2) NOT NULL,
+                         zipcode VARCHAR(10)
+);
--- src/lab7/prob2/Main.java
+++
@@ -1,8 +0,0 @@
-package lab7.prob2;
-
-public class Main {
-
-    public static void main(String[] args) {
-    }
-
-}
